a876f3c73af1380803a81959d06f5c5b416b89ea
eranga herath <eranga.herath@pagero.com>
2017-02-13 14:44:12 +0530

    simplify docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # apply database migrations
+# didn't use 'south' in order to keep things simple 
+# --noinput used to sync without creating superuser
 python manage.py syncdb --noinput
-#python manage.py migrate tastypie
-#python manage.py migrate api
 
 # start server
 python manage.py runserver 0.0.0.0:8000
